f005e47e4106c00ebb0763e47712aedbcd363910
rainavyas <sayvaniar@gmail.com>
2020-10-13 14:54:46 +0100

    pkl2pdf separate stage

--- training.py
+++ training.py
@@ -1,91 +1,19 @@
 import torch
-import numpy as np
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 from models import FCC
 import pickle
 
 
-def get_phones(alphabet='arpabet'):
-    if alphabet == 'arpabet':
-        vowels = ['aa', 'ae', 'eh', 'ah', 'ea', 'ao', 'ia', 'ey', 'aw', 'ay', 'ax', 'er', 'ih', 'iy', 'uh', 'oh', 'oy', 'ow', 'ua', 'uw']
-        consonants = ['el', 'ch', 'en', 'ng', 'sh', 'th', 'zh', 'w', 'dh', 'hh', 'jh', 'em', 'b', 'd', 'g', 'f', 'h', 'k', 'm', 'l', 'n', 'p', 's', 'r', 't', 'v', 'y', 'z'] + ['sil']
-        phones = vowels + consonants
-        return phones
-    if alphabet == 'graphemic':
-        vowels = ['a', 'e', 'i', 'o', 'u']
-        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] + ['sil']
-        phones = vowels + consonants
-        return phones
-    raise ValueError('Alphabet name not recognised: ' + alphabet)
-
-
-def get_pdf(obj, phones):
-    n = len(obj['plp'][0][0][0][0][0]) # dimension of mfcc vector
-
-    # Define the tensors required by spectral attack model
-    p_means = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , n))
-    p_covariances = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5), n, n))
-    q_means = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , n))
-    q_covariances = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5), n, n))
-    num_phones_mask = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5)))
-
-
-    for spk in range(len(obj['plp'])):
-        SX = np.zeros((len(phones) - 1, n, 1))
-        N = np.zeros(len(phones) - 1)
-        SX2 = np.zeros((len(phones) - 1, n, n))
-        Sig = np.zeros((len(phones) - 1, n, n))
-
-        for utt in range(len(obj['plp'][spk])):
-            for w in range(len(obj['plp'][spk][utt])):
-                for ph in range(len(obj['plp'][spk][utt][w])):
-                    for frame in range(len(obj['plp'][spk][utt][w][ph])):
-                        N[obj['phone'][spk][utt][w][ph]] += 1
-                        X = np.reshape(np.array(obj['plp'][spk][utt][w][ph][frame]), [n, 1])
-                        SX[obj['phone'][spk][utt][w][ph]] += X
-                        SX2[obj['phone'][spk][utt][w][ph]] += np.matmul(X, np.transpose(X))
-
-        for ph in range(len(phones)-1):
-            if N[ph] !=0:
-                SX[ph] /= N[ph]
-                SX2[ph] /= N[ph]
-            m2 = np.matmul(SX[ph], np.transpose(SX[ph]))
-            Sig[ph] = SX2[ph] - m2
-
-        k = 0
-        for i in range(len(phones) - 1):
-            for j in range(i + 1, len(phones) - 1):
-                if N[i] == 0 or N[j] == 0:
-                    num_phones_mask[spk][k] = 0
-                    # define Gaussian distribution that has 0 kl div
-                    # later the mask will be used to make these features "-1"
-                    p_covariances[spk][k] = np.eye(n)
-                    q_covariances[spk][k] = np.eye(n)
-                else:
-                    num_phones_mask[spk][k] = 1
-                    p_covariances[spk][k] = Sig[i]
-                    q_covariances[spk][k] = Sig[j]
-
-                p_means[spk][k] = SX[i].squeeze()
-                q_means[spk][k] = SX[j].squeeze()
-
-                k += 1
-
-    return p_means, p_covariances, q_means, q_covariances, num_phones_mask
-
-
-pkl_file = '/home/alta/BLTSpeaking/exp-vr313/data/mfcc13/GKTS4-D3/grader/BLXXXgrd02/BLXXXgrd02.pkl'
-pkl = pickle.load(open(pkl_file, "rb"))
-
-print("loaded pkl")
-
-# get the phones
-phones = get_phones()
-num_features = int((len(phones)-1)(len(phones)-2)*0.5)
-
-# get the means and covariances split into p and q groups (for doing kl)
-p_means, p_covariances, q_means, q_covariances, mask = get_pdf(pkl, phones)
+# Load the means and covariances
+input_file = 'BLXXXgrd02_means_covs.npz'
+npzfile = np.load(input_file)
+p_means = npzfile['arr_0']
+p_covariances = npzfile['arr_1']
+q_means = npzfile['arr_2']
+q_covariances = npzfile['arr_3']
+mask = npzfile['arr_4']
+y = npzfile['arr_5']
 
 print("got means and covs")
 
@@ -95,6 +23,7 @@ p_covariances = torch.from_numpy(p_covariances)
 q_means = torch.from_numpy(q_means)
 q_covariances = torch.from_numpy(q_covariances)
 mask = torch.from_numpy(mask)
+y = torch.from_numpy(y)
 
 
 # Define constants
@@ -103,10 +32,6 @@ epochs = 400
 bs = 20
 seed = 1
 torch.manual_seed(seed)
-
-# Construct the output scores tensor
-y = (pkl['score'])
-y = torch.FloatTensor(y)
 
 # Split into training and dev sets
 num_dev = 100
