cd72ba696900b796da1624f5ed5323447834593e
rainavyas <sayvaniar@gmail.com>
2020-10-22 23:39:38 +0100

    pkl to lens and mfcc vects all

--- pkl2pqvects.py
+++ pkl2pqvects.py
@@ -18,59 +18,47 @@ def get_phones(alphabet='arpabet'):
 def get_vects(obj, phones, max_num_mfccs_length):
     n = len(obj['plp'][0][0][0][0][0]) # dimension of mfcc vector
 
-    lengths = []
-
     # Define the tensors required by spectral attack model
-    p_vects = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , n))
-    q_vects = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , n))
+    p_vects = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , max_num_mfccs_length, n))
+    q_vects = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5) , max_num_mfccs_length, n))
     num_phones_mask = np.zeros((len(obj['plp']), int((len(phones)-1)*(len(phones)-2)*0.5)))
-    #mfcc_lenghts =
+    p_mfcc_lengths = np.zeros((len(obj['plp']),  int((len(phones)-1)*(len(phones)-2)*0.5))
+    q_mfcc_lengths = np.zeros((len(obj['plp']),  int((len(phones)-1)*(len(phones)-2)*0.5))
 
 
     for spk in range(len(obj['plp'])):
-        SX = np.zeros((len(phones) - 1, n, 1))
+        Xs = np.zeros((len(phones) - 1, max_num_mfccs_length, n))
         N = np.zeros(len(phones) - 1)
-        SX2 = np.zeros((len(phones) - 1, n, n))
-        Sig = np.zeros((len(phones) - 1, n, n))
 
         for utt in range(len(obj['plp'][spk])):
             for w in range(len(obj['plp'][spk][utt])):
                 for ph in range(len(obj['plp'][spk][utt][w])):
+                    # n.b. this is iterating through the phones that occur sequentially in a word
                     for frame in range(len(obj['plp'][spk][utt][w][ph])):
                         N[obj['phone'][spk][utt][w][ph]] += 1
-                        X = np.reshape(np.array(obj['plp'][spk][utt][w][ph][frame]), [n, 1])
-                        SX[obj['phone'][spk][utt][w][ph]] += X
-                        SX2[obj['phone'][spk][utt][w][ph]] += np.matmul(X, np.transpose(X))
+                        X = np.array(obj['plp'][spk][utt][w][ph][frame])
+                        Xs[obj['phone'][spk][utt][w][ph]][N[obj['phone'][spk][utt][w][ph]] - 1] = X
 
-        for ph in range(len(phones)-1):
-            lengths.append(N[ph])
-            print(N[ph])
-            if N[ph] !=0:
-                SX[ph] /= N[ph]
-                SX2[ph] /= N[ph]
-            m2 = np.matmul(SX[ph], np.transpose(SX[ph]))
-            Sig[ph] = SX2[ph] - m2
-    print("largest", max(lengths))
-        # k = 0
-        # for i in range(len(phones) - 1):
-        #     for j in range(i + 1, len(phones) - 1):
-        #         if N[i] == 0 or N[j] == 0:
-        #             num_phones_mask[spk][k] = 0
-        #             # define Gaussian distribution that has 0 kl div
-        #             # later the mask will be used to make these features "-1"
-        #             p_covariances[spk][k] = np.eye(n)
-        #             q_covariances[spk][k] = np.eye(n)
-        #         else:
-        #             num_phones_mask[spk][k] = 1
-        #             p_covariances[spk][k] = Sig[i]
-        #             q_covariances[spk][k] = Sig[j]
-        #
-        #         p_means[spk][k] = SX[i].squeeze()
-        #         q_means[spk][k] = SX[j].squeeze()
-        #
-        #         k += 1
 
-    return p_means, p_covariances, q_means, q_covariances, num_phones_mask
+        # Consturct every unique pairing of phones, related mfcc vectors
+        k = 0
+        for i in range(len(phones) - 1):
+            for j in range(i + 1, len(phones) - 1):
+                if N[i] == 0 or N[j] == 0:
+                    # define Gaussian distribution that has 0 kl div
+                    # later the mask will be used to make these features "-1"
+                    num_phones_mask[spk][k] = 0
+                else:
+                    num_phones_mask[spk][k] = 1
+
+                p_vects[spk][k] = Xs[i].squeeze()
+                q_vects[spk][k] = Xs[j].squeeze()
+                p_mfcc_lengths[spk][k] = N[i]
+                q_mfcc_lengths[spk][k] = N[j]
+
+                k += 1
+
+    return p_vects, q_vects, p_mfcc_lengths, q_mfcc_lengths, num_phones_mask
 
 
 pkl_file = '/home/alta/BLTSpeaking/exp-vr313/data/mfcc13/GKTS4-D3/grader/BLXXXgrd02/BLXXXgrd02.pkl'
@@ -83,14 +71,14 @@ print("loaded pkl")
 phones = get_phones()
 
 # get the mfcc vects as  [batch_size x 1128 x max_mfccs_per_phone x mfcc_dim] split into p and q groups (for doing kl)
-max_num_mfccs_length = 100
-p_vects, q_vects, mask, lengths = get_vects(pkl, phones, max_num_mfccs_length)
+max_num_mfccs_length = 4000
+p_vects, q_vects, p_lengths, q_lengths, mask = get_vects(pkl, phones, max_num_mfccs_length)
 
 # get output labels
 y = (pkl['score'])
 y = np.array(y)
 
-# # write to output file
-# output_file = 'BLXXXgrd02_pqvects.npz'
-# #output_file = 'BLXXXeval3_pqvects.npz'
-# np.savez(output_file, p_vects, q_vects, mask, lengths, y)
+# write to output file
+output_file = 'BLXXXgrd02_pqvects.npz'
+#output_file = 'BLXXXeval3_pqvects.npz'
+np.savez(output_file, p_vects, q_vects, mask, lengths, y)
