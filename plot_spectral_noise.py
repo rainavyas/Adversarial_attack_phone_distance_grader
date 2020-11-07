0411b966e4f532f6702537f8590f46587db0fbfe
V. Raina <vr313@air208.eng.cam.ac.uk>
2020-11-07 00:27:02 +0000

    plot works

--- plot_spectral_noise.py
+++ plot_spectral_noise.py
@@ -3,6 +3,8 @@ import pickle
 from attack_models import Spectral_attack
 import numpy as np
 import torch
+import torch_dct as dct
+
 
 def get_phones(alphabet='arpabet'):
     if alphabet == 'arpabet':
@@ -17,11 +19,11 @@ def get_phones(alphabet='arpabet'):
         return phones
     raise ValueError('Alphabet name not recognised: ' + alphabet)
 
-def spectral_convert(X):
+def spectral_convert(X, num_channels):
     X = torch.from_numpy(X).float()
     X_sq = X.squeeze()
     # Pad to spectral dimension
-    padding = torch.zeros(attack.size(0) - X_sq.size(0))
+    padding = torch.zeros(num_channels - X_sq.size(0))
     padded_X = torch.cat((X_sq, padding))
 
     # Apply inverse dct
@@ -35,7 +37,7 @@ def spectral_convert(X):
 
     return spectral_X
 
-def get_spectral_vects(obj, spk, phone):
+def get_spectral_vects(obj, spk, phone, num_channels):
     spectral_vects = []
 
     for utt in range(len(obj['plp'][spk])):
@@ -46,11 +48,11 @@ def get_spectral_vects(obj, spk, phone):
                 if curr_phone != phone:
                     continue
                 for frame in range(len(obj['plp'][spk][utt][w][ph])):
-                    X = np.reshape(np.array(obj['plp'][spk][utt][w][ph][frame]), [n, 1])
-                    X_spectral = spectral_convert(X)
+                    X = np.array(obj['plp'][spk][utt][w][ph][frame])
+                    X_spectral = spectral_convert(X, num_channels)
                     spectral_vects.append(X_spectral.tolist())
 
-return spectral_vects
+    return spectral_vects
 
 pkl_file = '/home/alta/BLTSpeaking/exp-vr313/data/mfcc13/GKTS4-D3/grader/BLXXXgrd02/BLXXXgrd02.pkl'
 #pkl_file = '/home/alta/BLTSpeaking/exp-vr313/data/mfcc13/GKTS4-D3/grader/BLXXXeval3/BLXXXeval3.pkl'
@@ -67,11 +69,12 @@ attack = attack.detach().numpy()
 # Choose speaker and phone to see channel energies for
 spk = 3
 phone = 10
+num_channels = 24
 
 phones = get_phones()
 phone_letters = phones[phone]
 
-spectral_vects = get_spectral_vects(obj, spk, phone)
+spectral_vects = get_spectral_vects(pkl, spk, phone, num_channels)
 spectral_vects = np.array(spectral_vects)
 
 # Get the attacked spectral vectors too
@@ -89,18 +92,18 @@ channel_means = channel_means.tolist()
 channel_stds = channel_stds.tolist()
 attacked_channel_means = attacked_channel_means.tolist()
 
-num_channels = 24
 ind = np.arange(num_channels)
-width = 0.35
+width = 0.45
 x = [str(i) for i in range(num_channels)]
-plt.bar(ind, channel_means, width, colour='blue', yerr=channel_stds, label='Spectral Vectors')
-plt.bar(ind+width, attacked_channel_means, width, colour='red', label='Attacked Spectral Vectors')
+plt.bar(ind, channel_means, width, color='blue', yerr=channel_stds, label='Spectral Vectors Mean')
+plt.bar(ind+width, attacked_channel_means, width, color='red', label='Attacked Spectral Vectors Mean')
 plt.xticks(ind+width/2, x)
 plt.xlabel("Spectrum Channel")
 plt.ylabel("Spectral Energy")
-plt.title("BLXXXgrd02 Data: Speaker = " + str(spk)+ ", Phone = "+str(phone) + " ("+phone_letters+")")
+plt.title("BLXXXgrd02 Data: Speaker = " + str(spk)+ ", Phone = "+str(phone) + " ('"+phone_letters+"')")
 plt.legend(loc='best')
+plt.ylim([0,1000])
 
 # Save the figure as an image
 plt.tight_layout()
-plt.savefig('Spectral Plot')
+plt.savefig('Spectral_Plot')
